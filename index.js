a30adf200eead198b32ec65c7fbd630c90b4d844
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-11-02 11:06:24 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var opcion = 0;
+
+if(opcion == 1){
+    var numeroacomprobar = 11;
+
+    if (numeroacomprobar == 10){
+        console.log(`su numero es un ${numeroacomprobar}`)
+    }else{
+        if(numeroacomprobar > 10){
+            console.log(`el numero ${numeroacomprobar} es mayor a 10`)
+        }else{
+            console.log(`el numero ${numeroacomprobar} es menor a 10`)
+        }
+    }
+}else{
+    if(opcion ==2){
+        var a = 10;
+        var b = 15;
+        console.log(`la resopuesta es ${a + b}`)
+    }else{
+        if(opcion == 3){
+            
+var diasem = 6;
+
+if(diasem == 1){
+    console.log('lunes')
+}else{
+    if(diasem == 2){
+        console.log('martes')
+    }else{
+        if(diasem == 3){
+            console.log('miercoles')
+        }else{
+            if(diasem == 4){
+                console.log('jueves')
+            }else{
+                if (diasem == 5) {
+                    console.log('viernes')
+                } else {
+                    if (diasem == 6) {
+                        console.log('sabado')
+                    } else {
+                        if (diasem == 7) {
+                            console.log('domingo')
+                        } else {
+                            console.log('escoga un nuemro valido del 1 al 7')
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+        }else{
+            console.log(`ingrese un numero valido`)
+        }
+    }
+}
